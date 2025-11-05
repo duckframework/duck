6f5d9c10f61866e7f62c74c4546bd7f5579ed42f
Brian Musakwa <digreatbrian@gmail.com>
2025-11-05 20:03:32 +0200

    Compatibility fixes

--- duck/compat.py
+++ duck/compat.py
@@ -7,5 +7,5 @@ def apply_backward_compatibility():
     # Patch datetime module
     # Only patch if Python < 3.11
     if sys.version_info < (3, 11):
-        import datetime, timezone
-        datetime.UTC = timezone.utc
+        import datetime
+        datetime.UTC = datetime.timezone.utc
