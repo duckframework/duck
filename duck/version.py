a1de65e54cf8fd1a63537967355d7adac1eda44b
Brian Musakwa <digreatbrian@gmail.com>
2026-07-15 23:17:03 +0200

    Improve version module

--- duck/version.py
+++ duck/version.py
@@ -1,10 +1,26 @@
 """
-Version module for the Duck package.
+Version information for the Duck package.
 """
+
 import sys
 
 
-version_num = (2, 2, 0)
-version = "%d.%d.%d" % version_num
+# Package version
+version_info = (2, 2, 0)
+version = ".".join(map(str, version_info))
 
+# Server identification string
+python_version = ".".join(map(str, sys.version_info[:3]))
+server_version = f"Duck v{version} Python/{python_version}"
+
+# Public package version
 __version__ = version
+
+
+# Expose some tools
+__all__ = [
+    "version_info",
+    "version",
+    "server_version",
+    "__version__",
+]
